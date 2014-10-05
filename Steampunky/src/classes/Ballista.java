02ab5c5dc26c7b23deb482cc605958e81935089a
nick van der mullen <Nickvdmullen@gmail.com>
2014-10-05 14:07:25 +0200

    First Code of Ballista/Direction/Projectile
    Made constructors and methodes/getters of things we had in ClassDiagram

--- Steampunky/src/classes/Ballista.java
+++ Steampunky/src/classes/Ballista.java
@@ -10,39 +10,93 @@ import java.util.*;
 
 /**
  * OK
- * @author Melanie
+ * @author Nick van der Mullen
  */
 public class Ballista extends Object
 {    
     //************************datavelden*************************************
     private int ballistaID;
     private int nextBallistaID = 1;
-    private String direction;
+    private Direction direction;
+    private String ballistaType;
     private int shots;
+    private int shotsShot;
     private int projectileSpeed;
-    private Projectile projectile;
 
-    //***********************constructoren***********************************
     /**
-     * creates a ballista with ...
-     */
-    public Ballista(String direction, int shots, int projectileSpeed)
+     * The constructor of the Object Ballista, 
+     * when this object is made a number of Projectiles is made to match shots.
+     * Depending on the direction give it depends which direction is made first.
+     * 
+     * @param type              The type thats give so the ballista knows what type of Projectiles is must make.
+     * @param shots             The number of shots a ballista can fire.
+     * @param projectileSpeed   The speed of the Projectiles the ballista makes.
+     * @param positionX         An attribute of Object that sets the  X position that the Ballista will be placed on.
+     * @param positionY         An attribute of Object that sets the  Y position that the Ballista will be placed on.
+     * @param active            An attribute of Object that sets if an Object is active or not.
+     * @param movable           An attribute of Object that sets if an Object is movable or not.
+     * @param direction         An attribute of Object that is used to decide which way to shoot first.
+    **/
+    public Ballista(String type, int shots, int projectileSpeed,int positionX,int positionY,boolean active,boolean movable,Direction direction)
     {
+        super(positionX,positionY,movable,direction,active);
         this.ballistaID = this.nextBallistaID;
         this.nextBallistaID++;
+        this.ballistaType = type;
         this.direction = direction;
         this.shots = shots;
+        this.shotsShot = 0;
         this.projectileSpeed = projectileSpeed;
+        
+        while(shotsShot < shots)
+        {
+            if(direction == Direction.Up || this.direction == Direction.Down)
+            {
+                createProjectile(Direction.Up);
+                createProjectile(Direction.Down);
+                createProjectile(Direction.Left);
+                createProjectile(Direction.Right);
+            }
+            else
+            {
+                createProjectile(Direction.Left);
+                createProjectile(Direction.Right); 
+                createProjectile(Direction.Up);
+                createProjectile(Direction.Down);
+            }
+
+        }
+        
     }
 
     //**********************methoden****************************************
+    
+    /**
+     * The getter of the Ballista ID.
+     * @return  Returns an integer that represents the Ballista ID.
+     */
     public int getBallistaID()
     {
         return this.ballistaID;
     }
     
-    public void createProjectile()
+    /**
+     * The getter of shots.
+     * @return  Returns an integer with the amount of shots the Ballista can fire.
+     */
+    public int getShots()
     {
-        //todo
+        return this.shots;
     }
+    
+    /**
+     * An Method that creates a projectile with the given direction.
+     * @param direction     The direction the projectile will go.
+     */
+    public void createProjectile(Direction direction)
+    {            
+            Projectile newProjectile = new Projectile(this.ballistaType,this.projectileSpeed,this.getPositionX(),this.getPositionY(),false,false,direction);            
+    }
+    
+   
 }
--- Steampunky/src/classes/Projectile.java
+++ Steampunky/src/classes/Projectile.java
@@ -10,29 +10,110 @@ import java.util.*;
 
 /**
  * OK
- * @author Melanie
+ * @author Nick van der Mullen
  */
 public class Projectile extends Object
 {
     //************************datavelden*************************************
-    private int projectileID;
+    private Integer projectileID ;
+    private Integer nextProjectileID = 1;
     private String type;
     private double speed;
-    private String direction;
+    private Direction direction;
 
-    //***********************constructoren***********************************
     /**
-     * creates a projectile with ...
+     * The constructor for making a Object of the Type Projectile
+     * @param type          the type of the projectile f.ex slow/fast
+     * @param speed         the speed of the projectile.
+     * @param positionX     Attribute of Object (the X position of the projectile)
+     * @param positionY     Attribute of Object (the Y position of the projectile)
+     * @param active        Attribute of Object (a boolean to check if the projectile is on screen)
+     * @param movable       Attribute of Object (a boolean to check if the object can move)
+     * @param direction     Attribute of Object (the direction of the projectile)
      */
-    public Projectile()
+    public Projectile(String type,double speed,int positionX,int positionY,boolean active,boolean movable,Direction direction)
     {
-        //todo
+        super(positionX,positionY,active,movable,direction);
+        
+        this.projectileID = nextProjectileID;
+        this.type = type;
+        this.speed = speed;
+        this.direction = direction;
+        this.nextProjectileID++;
     }
 
     //**********************methoden****************************************
-    public Object checkCollision()
+    
+    /**
+     * Getter for Type
+     * @return  Returns a string with the type of the projectile
+     */
+    public String getType()
     {
-        //todo
-        return null;
+        return this.type;
     }
+    
+    /**
+     * Getter for Speed
+     * @return  Returns a double with the speed of the projectile
+     */
+    public double getSpeed()
+    {
+        return this.speed;
+    }
+    
+    /**
+     * Getter for Direction
+     * @return  Returns a Direction with the direction the projectile is moving
+     */
+    public Direction getDirection()
+    {
+        return this.direction;
+    }
+    
+    /**
+     * An method to check if the next Position of this projectile will collide with another object.
+     * @param objects   An list of all object currently in the game.
+     * @return          Returns an object if the next position of this projectile collides with the object
+     *                  else it returns null.
+     */
+    public Object checkCollision(List<Object> objects)
+    {
+        int tempPositionX = this.getPositionX();
+        int tempPositionY = this.getPositionY();
+        Object hitObject = null;
+        
+        if(direction == direction.Right)
+        {
+            tempPositionX++;
+        }
+        else if(direction == direction.Left)
+        {
+            tempPositionX--;
+        }
+        else if(direction == direction.Up)
+        {
+            tempPositionY++;
+        }
+        else
+        {
+            tempPositionY--;
+        }
+        
+        for(Object nxtObject : objects)
+        {
+            int oPositionX = nxtObject.getPositionX();
+            int oPositionY = nxtObject.getPositionY();
+            
+            if((tempPositionX == oPositionX) && (tempPositionY == oPositionY))
+            {
+                hitObject = nxtObject;
+                break;
+            }
+            
+        }
+        return hitObject;
+    }
+    
+    
 }
